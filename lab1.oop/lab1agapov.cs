8657db212378395662b558c60d9c6213d85f9ad3
sashaagapov <sasha20070315@gmail.com>
2026-02-10 10:29:27 +0200

    Start of lab1

--- lab1.oop/lab1agapov.cs
+++ lab1.oop/lab1agapov.cs
@@ -1,70 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
-namespace RandomCodeNamespace
+namespace lab1agapov
 {
-    public class RandomGenerator
+    class Program
+{
+    static void Main(string[] args)
     {
-        private Random _random = new Random();
+        Console.WriteLine("=== Завдання 1: Анкетні дані ===");
+            StudentInfo();
 
-        public void GenerateRandomNumbers()
-        {
-            Console.WriteLine("=== Random Numbers ===");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"Number {i + 1}: {_random.Next(1, 100)}");
-            }
-        }
+            Console.WriteLine("\n=== Завдання 2: Обчислення поліному ===");
+            CalculatePolynomial();
 
-        public string GenerateRandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            return new string(Enumerable.Range(0, length)
-                .Select(_ => chars[_random.Next(chars.Length)])
-                .ToArray());
-        }
-
-        public List<int> GenerateRandomList(int size, int maxValue)
-        {
-            return Enumerable.Range(0, size)
-                .Select(_ => _random.Next(0, maxValue))
-                .ToList();
-        }
-
-        public double CalculateRandomStatistics()
-        {
-            var numbers = GenerateRandomList(50, 1000);
-            double average = numbers.Average();
-            double sum = numbers.Sum();
-            double max = numbers.Max();
-            double min = numbers.Min();
-
-            Console.WriteLine($"Average: {average:F2}");
-            Console.WriteLine($"Sum: {sum}");
-            Console.WriteLine($"Max: {max}");
-            Console.WriteLine($"Min: {min}");
-
-            return average;
-        }
+            // Щоб консоль не закрилася одразу після виконання
+            Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+            Console.ReadKey();
     }
-
-    public class Program
-    {
-        static void Main(string[] args)
+    static void StudentInfo()
         {
-            var generator = new RandomGenerator();
-
-            generator.GenerateRandomNumbers();
-            Console.WriteLine("\n=== Random String ===");
-            Console.WriteLine($"Random String (15 chars): {generator.GenerateRandomString(15)}");
-
-            Console.WriteLine("\n=== Statistics ===");
-            generator.CalculateRandomStatistics();
-
-            Console.WriteLine("\n=== Random List ===");
-            var list = generator.GenerateRandomList(10, 500);
-            Console.WriteLine($"Random List: [{string.Join(", ", list)}]");
+            ConsoleWriteLine("---------------------------------------------------");
+            Console.WriteLine("Агапов Олександр,ІПЗ-11(1),лабораторна робота №1");
+            ConsoleWriteLine("---------------------------------------------------");
         }
-    }
+        static void CalculatePolynomial()
+        {
+            double coefficientA = 2.5;
+            double coefficientB = 1.2;
+            double coefficientC = -4.0;
+            double freeTermD = 10.5;
+            double resultP = 0;
+            Console.WriteLine("Введіть значення x: ");
+
+            double argumentX = double.Parse(Console.WriteLine());
+            Console.WriteLine("Рівняння:  p = a*x^5 − 1 / b*x^4+c*x + d.");
+
+            resultP = coefficientA * Math.Pow(argumentX,5) - (1 /coefficientB * Math.Pow(argumentX, 4 )) + coefficientC * argumentX + freeTermD;
+
+            Console.WriteLine($"Результат(p) = {resultP}");
+        }
+}
 }
